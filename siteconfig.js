0772f8ba319edbca4e6a02adb45192f0da834786
jomurgel <dev@jomurgel.com>
2017-12-05 22:53:59 -0700

    adds siteconfig.js — placeholder content

---
+++ siteconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  PORT: '8080',
+  TITLE: 'Project Acorn | Vue + Wordpress',
+  DESCRIPTION: 'Default Site Description',
+  CARD_IMAGE: 'http://placehold.it/1280x768/f60/fff?text=HOME',
+  BASE_URL: 'http://wordpress.dev/wp-json/',
+  ICON: 'acorn.png', // roots to ./public/ folder
+  AUTHOR: 'jomurgel',
+  AUTHOR_URL: 'https://jomurgel.com'
+}
