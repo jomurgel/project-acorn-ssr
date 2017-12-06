a7151ef680871dc870836d3a6ec786db8bded37c
jomurgel <dev@jomurgel.com>
2017-12-06 09:21:40 -0700

    updates siteconfig.js for testing

--- siteconfig.js
+++ siteconfig.js
@@ -2,8 +2,8 @@ module.exports = {
   PORT: '8080',
   TITLE: 'Project Acorn | Vue + Wordpress',
   DESCRIPTION: 'Default Site Description',
-  CARD_IMAGE: 'http://placehold.it/1280x768/f60/fff?text=HOME',
-  BASE_URL: 'http://wordpress.dev/wp-json/',
+  CARD_IMAGE: 'http://placehold.it/1280x768/ee2d6d/fff?text=PROJECT%20ACORN',
+  BASE_URL: 'https://vue-app.jomurgel.com/server/wp-json/',
   ICON: 'acorn.png', // roots to ./public/ folder
   AUTHOR: 'jomurgel',
   AUTHOR_URL: 'https://jomurgel.com'
