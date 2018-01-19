e70209f2dda0daf1118638ad04ae092c4fe06da7
jomurgel <dev@jomurgel.com>
2018-01-19 09:48:24 -0700

    adds title output for svgs and capitalizes

--- src/views/common/_Icon.vue
+++ src/views/common/_Icon.vue
@@ -1,5 +1,6 @@
 <template>
   <svg :class="'icon icon-' + name" :width="width" :height="height">
+    <title>{{ name.charAt( 0 ).toUpperCase() + name.slice( 1 ) }}</title>
     <use xmlns:xlink="https://www.w3.org/1999/xlink" :xlink:href="'#i-' + name"></use>
   </svg>
 </template>
