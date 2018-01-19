f6116069e9009f2b74cbb3cccf6465eca4c63829
jomurgel <dev@jomurgel.com>
2018-01-19 13:14:14 -0700

    adds function to display title, description and featured image

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,14 +1,11 @@
+import { CARD_IMAGE } from '@root/webconfig'
+
 // Get the menu associated with a slug.
 export function returnQueriedMenu( array, slug ) {
 
   // Setup vars
   let menu         = array
   let menuLocation = slug
-
-  // Return if empty.
-  if ( ! menu ) {
-    return
-  }
 
   // Loop through menu items and grab array where slug = menu location.
   const newMenu = menu.filter( element => element.slug === menuLocation )
@@ -21,4 +18,38 @@ export const modifier = {
   posts: 'wp/v2/posts',
   menus: 'wp-api-menus/v2/menus',
   pages: 'wp/v2/pages'
+}
+
+export function setTitle( data ) {
+
+  // Assuming we have data, return WordPress title, else 404.
+  // Helpful for error handling components.
+  return data ? data.title : 'Oops! a 404'
+}
+
+export function setDescription( data ) {
+
+  // Assuming we have data, return WordPress title, else 404.
+  // Helpful for error handling components.
+  return data ? data.excerpt.replace( /<(?:.|\n)*?>/gm, '' ).split( /\s+/ ).slice( 0, 15 ).join( ' ' ) + '...' : 'Looks like something\'s gone wrong.'
+}
+
+export function setFeaturedImage( data ) {
+
+  // Get root to make sure we have a featured image.
+  let source = data ? data.featured_image : ''
+
+  // Double check else output our default set in webconfig.js.
+  source = source ? source.media.sizes.medium.source_url : CARD_IMAGE
+
+  return source
+}
+
+export function setSinglePost( slug, posts ) {
+
+  const post = posts.filter( function( e ) {
+    return e.slug === slug
+  })
+
+  return post[0]
 }
