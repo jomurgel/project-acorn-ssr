343d061f057117d7725ee246d0e9452b9aea5755
jomurgel <dev@jomurgel.com>
2018-01-19 08:37:57 -0700

    renames siteconfig to webconfig

---
+++ webconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  PORT: '8080',
+  TITLE: 'Project Acorn | Vue + Wordpress',
+  DESCRIPTION: 'Default Site Description',
+  CARD_IMAGE: 'https://placehold.it/1280x768/ee2d6d/fff?text=PROJECT%20ACORN',
+  SITE_URL: 'https://localhost:8080',
+  BASE_URL: 'https://vue-app.jomurgel.com/server', // no trailing slash
+  ICON: 'acorn.png', // roots to ./public/ folder
+  AUTHOR: 'jomurgel',
+  AUTHOR_URL: 'https://jomurgel.com'
+}
