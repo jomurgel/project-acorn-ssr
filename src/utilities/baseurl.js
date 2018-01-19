2de5544b3771b91a8aa4e8124a4663fa7529ba84
jomurgel <dev@jomurgel.com>
2018-01-19 13:10:06 -0700

    changes siteconfig to webconfig

--- src/utilities/baseurl.js
+++ src/utilities/baseurl.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { BASE_URL } from '@root/siteconfig'
+import { BASE_URL } from '@root/webconfig'
 
 export const HTTP = axios.create({
   baseURL: BASE_URL + '/wp-json/'
