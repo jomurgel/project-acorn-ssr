20bad383a7d56a4563810a8c1fe83fe725c9172d
jomurgel <dev@jomurgel.com>
2018-01-21 16:18:57 -0700

    removes modifier const variable and moves to api folder in locations.js

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,17 +1,6 @@
 import { CARD_IMAGE } from '@root/webconfig'
 
 /**
- * Set api locations for posts, menus, and pages.
- *
- * @export variable modifier
- */
-export const modifier = {
-  posts: 'wp/v2/posts',
-  menus: 'wp-api-menus/v2/menus',
-  pages: 'wp/v2/pages'
-}
-
-/**
  * Get the menu associated with a slug.
  *
  * @export function
