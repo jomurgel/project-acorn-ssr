268c9cf1b7aff8164184c8d9c6f2fa6ee5138138
jomurgel <dev@jomurgel.com>
2018-01-21 15:52:06 -0700

    removes Page.vue and SIngle.vue and combines into one reusable template file

--- src/views/Single.vue
+++ src/views/Single.vue
@@ -1,11 +1,68 @@
 <template>
-  <div>
-    <h1>Single Post</h1>
+  <div v-if="page" :key="page.id">
+    <h1>{{ title }}</h1>
+    <div v-html="page.content"></div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import {
+  setTitle,
+  setDescription,
+  setFeaturedImage,
+  setSinglePost } from '@src/utilities/helpers'
+
 export default {
-  title: 'Single'
+  meta() {
+    const meta = {
+      title: this.title,
+      description: this.description,
+      card: this.featuredImage
+    }
+    return meta
+  },
+  asyncData({ store, route }) {
+    if ( route.path.includes( 'blog' ) ) {
+      return store.dispatch( 'getPosts' )
+    } else {
+      return store.dispatch( 'getPages' )
+    }
+  },
+  computed: {
+    ...mapGetters({
+      pages: 'pages',
+      posts: 'posts'
+    }),
+    type: function() {
+      if ( this.$route.path.includes( 'blog' ) ) {
+        return this.posts
+      } else {
+        return this.pages
+      }
+    },
+    page: function() {
+      return setSinglePost( this.$route.params.slug, this.type )
+    },
+    title: function() {
+      return setTitle( this.page )
+    },
+    description: function() {
+      return setDescription( this.page )
+    },
+    featuredImage: function() {
+      return setFeaturedImage( this.page )
+    }
+  },
+  beforeRouteUpdate( to, from, next ) {
+    const pageArray = this.$store.state.pages
+    const test = pageArray.map( ( object ) => { return object.slug })
+
+    if ( test.indexOf( to.params.slug ) > -1 ) {
+      next()
+    } else {
+      next({ name: '404', params: { '0': to.path } })
+    }
+  }
 }
 </script>
