292b8882d2a8f2ca63f474777e9ef006985b2500
jomurgel <dev@jomurgel.com>
2018-01-21 16:18:44 -0700

    moves location and base url into api folder

---
+++ src/api/location.js
@@ -0,0 +1,10 @@
+/**
+ * Set api locations for posts, menus, and pages.
+ *
+ * @export variable modifier
+ */
+export const modifier = {
+  posts: 'wp/v2/posts',
+  menus: 'wp-api-menus/v2/menus',
+  pages: 'wp/v2/pages'
+}
