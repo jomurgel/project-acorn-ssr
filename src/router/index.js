51dcda6ef11a654886f81cffbf97990e1101a652
jomurgel <dev@jomurgel.com>
2018-01-21 15:54:41 -0700

    adds routes with beforeEnter check to 404 if necessary

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,66 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import { HTTP } from '../utilities/baseurl'
+import { modifier } from '../utilities/helpers'
 
 Vue.use( Router )
 
 const createView = relativePath => () => import( '@src/views/' + relativePath )
 
+const routes = [
+  {
+    path: '/',
+    component: createView( 'Custom' )
+  },
+  {
+    path: '/blog',
+    component: createView( 'Blog' )
+  },
+  {
+    path: '/blog/:slug',
+    component: createView( 'Single' ),
+    beforeEnter: ( to, from, next ) => {
+      HTTP.get( modifier.posts ).then( ( response ) => {
+        const postArray = response.data
+        const test = postArray.map( ( object ) => { return object.slug })
+
+        if ( test.indexOf( to.params.slug ) === -1 ) {
+          next({ name: '404' })
+        }
+        next()
+      })
+    }
+  },
+  {
+    path: '/:slug',
+    component: createView( 'Single' ),
+    beforeEnter: ( to, from, next ) => {
+      HTTP.get( modifier.pages ).then( ( response ) => {
+        const pageArray = response.data
+        const test = pageArray.map( ( object ) => { return object.slug })
+
+        if ( test.indexOf( to.params.slug ) === -1 ) {
+          next({ name: '404' })
+        }
+        next()
+      })
+    }
+  },
+  {
+    // Catch-all, though the 404 component is
+    // called in templates to prevent issues.
+    path: '/404',
+    name: '404',
+    component: createView( '404' )
+  },
+  {
+    path: '*',
+    redirect: '/404'
+  }
+]
+
 export default () => new Router({
   mode: 'history',
   scrollBehavior: () => ({ y: 0 }),
-  routes: [
-    {
-      path: '/',
-      component: createView( 'Home' )
-    },
-    {
-      path: '/blog',
-      component: createView( 'Posts' )
-    },
-    {
-      path: '/blog/:slug',
-      component: createView( 'Single' )
-    },
-    {
-      path: '/:slug',
-      component: createView( 'Page' )
-    },
-    {
-      path: '*',
-      component: createView( '404' )
-    }
-  ]
+  routes
 })
