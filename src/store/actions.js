6665f7a9b200ccde0b90fb3ae54c7ed889d1a765
jomurgel <dev@jomurgel.com>
2018-01-27 19:46:52 -0700

    adds check to only dispatch request if we don't already have data

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,46 +3,52 @@ import { modifier } from '../api/location'
 
 export default {
   getPages: ({ commit, state }) => {
-    return HTTP.get( modifier.pages ).then( ( response ) => {
-      if ( response.status === 200 && response.data.length > 0 ) {
-        const array = []
-        response.data.map( ( item ) => {
-          const filtered = {
-            id: item.id,
-            title: item.title.rendered,
-            excerpt: item.excerpt.rendered,
-            slug: item.slug,
-            content: item.content.rendered,
-            featured_image: item.featured_image
-          }
-          return array.push( filtered )
-        })
-        commit( 'setPages', array )
-      }
-    }).catch( ( error ) => {
-      console.log( error )
-    })
+    // Only get data if we don't already have it.
+    if ( state.pages.length === 0 ) {
+      return HTTP.get( modifier.pages ).then( ( response ) => {
+        if ( response.status === 200 && response.data.length > 0 ) {
+          const array = []
+          response.data.map( ( item ) => {
+            const filtered = {
+              id: item.id,
+              title: item.title.rendered,
+              excerpt: item.excerpt.rendered,
+              slug: item.slug,
+              content: item.content.rendered,
+              featured_image: item.featured_image
+            }
+            return array.push( filtered )
+          })
+          commit( 'setPages', array )
+        }
+      }).catch( ( error ) => {
+        console.log( error )
+      })
+    }
   },
   getPosts: ({ commit, state }) => {
-    return HTTP.get( modifier.posts ).then( ( response ) => {
-      if ( response.status === 200 && response.data.length > 0 ) {
-        const array = []
-        response.data.map( ( item ) => {
-          const filtered = {
-            id: item.id,
-            title: item.title.rendered,
-            excerpt: item.excerpt.rendered,
-            slug: item.slug,
-            content: item.content.rendered,
-            featured_image: item.featured_image
-          }
-          return array.push( filtered )
-        })
-        commit( 'setPosts', array )
-      }
-    }).catch( ( error ) => {
-      console.log( error )
-    })
+    // Only get data if we don't already have it.
+    if ( state.posts.length === 0 ) {
+      return HTTP.get( modifier.posts ).then( ( response ) => {
+        if ( response.status === 200 && response.data.length > 0 ) {
+          const array = []
+          response.data.map( ( item ) => {
+            const filtered = {
+              id: item.id,
+              title: item.title.rendered,
+              excerpt: item.excerpt.rendered,
+              slug: item.slug,
+              content: item.content.rendered,
+              featured_image: item.featured_image
+            }
+            return array.push( filtered )
+          })
+          commit( 'setPosts', array )
+        }
+      }).catch( ( error ) => {
+        console.log( error )
+      })
+    }
   },
   getMenus: ({ commit, state }) => {
     return HTTP.get( modifier.menus ).then( ( response ) => {
