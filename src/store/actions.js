07cb7d5b14e0bfbfa49215eea946e57cae365fe1
jomurgel <dev@jomurgel.com>
2018-02-05 18:30:25 -0700

    refactors page action (now pulls slug)

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,29 +2,40 @@ import { HTTP } from '../api/baseurl'
 import { modifier } from '../api/location'
 
 export default {
-  getPages: ({ commit, state }) => {
+  getPage: ({ commit, state }, slug ) => {
+    return state.pages[slug] ||
     // Only get data if we don't already have it.
-    if ( state.pages.length === 0 ) {
-      return HTTP.get( modifier.pages ).then( ( response ) => {
-        if ( response.status === 200 && response.data.length > 0 ) {
-          const array = []
-          response.data.map( ( item ) => {
-            const filtered = {
-              id: item.id,
-              title: item.title.rendered,
-              excerpt: item.excerpt.rendered,
-              slug: item.slug,
-              content: item.content.rendered,
-              featured_image: item.featured_image
-            }
-            return array.push( filtered )
-          })
-          commit( 'setPages', array )
-        }
-      }).catch( ( error ) => {
-        console.log( error )
-      })
-    }
+    HTTP.get( modifier.pages + '/?slug=' + slug ).then( ( response ) => {
+      if ( response.status === 200 && response.data.length > 0 ) {
+
+        // Set placeholder array.
+        const pageArray = []
+
+        // Remove unecessary data from object.
+        response.data.map( ( item ) => {
+          const filtered = {
+            id: item.id,
+            title: item.title.rendered,
+            excerpt: item.excerpt.rendered,
+            slug: item.slug,
+            content: item.content.rendered,
+            featured_image: item.featured_image
+          }
+
+          // Return new array object.
+          return pageArray.push( filtered )
+        })
+
+        // Get object in array.
+        const page = pageArray[0]
+        // Grab object slug.
+        const slug = page.slug
+
+        commit( 'setPage', { slug, page })
+      }
+    }).catch( ( error ) => {
+      console.log( error )
+    })
   },
   getPosts: ({ commit, state }) => {
     // Only get data if we don't already have it.
