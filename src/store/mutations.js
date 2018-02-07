f1b4406f7c8b833302338a156cf8050cd8679f06
jomurgel <dev@jomurgel.com>
2018-02-06 19:37:26 -0700

    adds mutations for pullstatus and pulltimestamp

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 
 export default {
-  setPost: ( state, { slug, post }) => {
-    Vue.set( state.posts, slug, post )
-  },
   setPosts: ( state, { posts }) => {
     posts.forEach( post => {
       if ( post ) {
         Vue.set( state.posts, post.slug, post )
       }
     })
+  },
+  setBlogPullStatus: ( state, blogPull ) => {
+    state.blogPull = blogPull
+  },
+  setBlogPullTimeStamp: ( state, blogPullDate ) => {
+    state.blogPullDate = blogPullDate
   },
   setPage: ( state, { slug, page }) => {
     Vue.set( state.pages, slug, page )
