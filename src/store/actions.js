707644295e106a4e0e87392133f84ed41cce1739
jomurgel <dev@jomurgel.com>
2018-02-06 19:37:12 -0700

    refactors blog/post actions to reuse cache/store over new request, timeout 24 hours

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,7 @@
 import { HTTP } from '../api/baseurl'
 import router from '../router'
 import { modifier } from '../api/location'
+import { ObjectSize } from '../utilities/helpers'
 
 export default {
   getPage: ({ commit, state }, slug ) => {
@@ -70,11 +71,9 @@ export default {
         })
 
         // Get object in array.
-        const post = postArray[0]
-        // Grab object slug.
-        const slug = post.slug
+        const posts = postArray
 
-        commit( 'setPost', { slug, post })
+        commit( 'setPosts', { posts })
       } else {
         // If we don't recieve data push to 404 post.
         router.push({ name: '404' })
@@ -85,11 +84,101 @@ export default {
   },
   getPosts: ({ commit, state }) => {
 
-    // Only get data if we don't already have it.
-    return state.posts.length > 10
-      ? state.posts.length
-      : HTTP.get( modifier.posts ).then( ( response ) => {
+    // Get post object length.
+    const postLength = ObjectSize( state.posts )
+
+    // Empty array of exclusions.
+    const arrayExclusion = []
+
+    // If we have any posts.
+    if ( postLength > 0 ) {
+
+      console.log( state.date, state.blogPullDate )
+
+      // If we've already pulled our posts and our pull date is within the last 24 hours, return state.
+      if ( state.blogPull === true && state.blogPullDate - state.date < 24 * 60 * 60 * 1000 ) {
+
+        // Return our cache.
+        return state.posts
+
+      } else {
+
+        // Get our post object.
+        const postObject = state.posts
+
+        // Loop through them and get ids for those that are older than 24hours.
+        Object.keys( postObject ).forEach( function( key ) {
+
+          let postID       = postObject[key].id
+          let now          = state.date
+          let modDate      = postObject[key].modifiedDate
+          let modifiedDate = ( new Date( modDate ) ).getTime()
+
+          // If our modified date is less than 24hours old push to array.
+          if ( now - modifiedDate < 24 * 60 * 60 * 1000 ) {
+            arrayExclusion.push( postID )
+          }
+
+        })
+
+        // Return a new comma-separated string else empty string.
+        const newArrayExclusion = arrayExclusion.length > 0 ? arrayExclusion.join( ',' ) : ''
+
+        // Then, let's make a request for all posts except for the ones
+        // we already have that aren't expired.
+        return HTTP.get( modifier.posts + '?exclude=' + newArrayExclusion ).then( ( response ) => {
+          if ( response.status === 200 && response.data.length > 0 ) {
+
+            // Set blog pull status to true.
+            commit( 'setBlogPullStatus', true )
+
+            // Set date of that pull.
+            commit( 'setBlogPullTimeStamp', ( new Date() ).getTime() )
+
+            // Set placeholder array.
+            const postArray = []
+
+            // Remove unecessary data from object.
+            response.data.map( ( post ) => {
+              const filtered = {
+                content: post.content.rendered,
+                excerpt: post.excerpt.rendered,
+                featuredImage: post.featured_image,
+                id: post.id,
+                modifiedDate: post.modified,
+                slug: post.slug,
+                title: post.title.rendered
+              }
+
+              // Return new array object.
+              return postArray.push( filtered )
+            })
+
+            // Get object in array.
+            const posts = postArray
+
+            commit( 'setPosts', { posts })
+          } else {
+            // If we don't recieve data push to 404 post.
+            router.push({ name: '404' })
+          }
+        }).catch( ( error ) => {
+          console.log( error )
+        })
+
+      }
+
+    } else {
+
+      // Only get data if we don't already have it.
+      return HTTP.get( modifier.posts ).then( ( response ) => {
         if ( response.status === 200 && response.data.length > 0 ) {
+
+          // Set blog pull status to true.
+          commit( 'setBlogPullStatus', true )
+
+          // Set date of that pull.
+          commit( 'setBlogPullTimeStamp', ( new Date() ).getTime() )
 
           // Set placeholder array.
           const postArray = []
@@ -121,6 +210,8 @@ export default {
       }).catch( ( error ) => {
         console.log( error )
       })
+    }
+
   },
   getMenus: ({ commit, state }) => {
     return HTTP.get( modifier.menus ).then( ( response ) => {
