321b23bb55ecd72c551fc180a16624b8dc2159d7
jomurgel <dev@jomurgel.com>
2018-02-08 18:24:01 -0700

    adds api function to return promise from pages or posts

---
+++ src/api/index.js
@@ -0,0 +1,41 @@
+import { HTTP } from '../api/baseurl'
+import router from '../router'
+
+// Return promise from post data.
+// Works with posts (all) and individual pages or posts.
+export function makePostRequest( url ) {
+
+  return HTTP.get( url ).then( ( response ) => {
+
+    if ( response.status !== 200 || response.data.length === 0 ) {
+
+      // If we don't recieve data push to 404 post.
+      router.push({ name: '404' })
+    }
+
+    const totalPostCount = response.headers['x-wp-total']
+
+    // Set placeholder array & Remove unecessary data from object.
+    const postArray = response.data.map( ( post ) => {
+      const filtered = {
+        content: post.content.rendered,
+        excerpt: post.excerpt.rendered,
+        featuredImage: post.featured_image,
+        id: post.id,
+        modifiedDate: post.modified,
+        slug: post.slug,
+        title: post.title.rendered,
+        totalPosts: totalPostCount
+      }
+
+      // Return new array object.
+      return filtered
+    })
+
+    // Get object in array.
+    return postArray
+
+  }).catch( ( error ) => {
+    console.log( error )
+  })
+}
