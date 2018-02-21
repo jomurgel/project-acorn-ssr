9e7c4eae73d3f39fc9da1a8289ea270437a9cfa6
jomurgel <dev@jomurgel.com>
2018-02-21 16:17:27 -0700

    adds link to home on 404 page.

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <h1>404</h1>
+    <h1>{{ title }}</h1>
+    <p>
+      Looks like the thing you're looking for isn't here.  Why not <router-link :to="{ path: '/' }">go home</router-link>?
+    </p>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      title: '404'
+    }
+  },
   meta() {
     const meta = {
-      title: '404',
+      title: this.title,
       description: 'Description for 404',
       card: 'https://placehold.it/1280x768/f60/fff?text=404',
       statusCode: 404
