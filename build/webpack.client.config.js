fd4d354afeceacebd66fdfa5882ed8c49c5123fb
jomurgel <dev@jomurgel.com>
2018-02-23 13:16:44 -0700

    moves service worker plugin to a production-only chunk

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,10 +1,11 @@
 const path               = require( 'path' )
 const webpack            = require( 'webpack' )
-const glob               = require( 'glob' )
 const merge              = require( 'webpack-merge' )
 const base               = require( './webpack.base.config' )
 const VueSSRClientPlugin = require( 'vue-server-renderer/client-plugin' )
 const SWPrecachePlugin   = require( 'sw-precache-webpack-plugin' )
+
+const isProduction  = process.env.NODE_ENV === 'production'
 
 const config = merge( base, {
   entry: {
@@ -27,6 +28,20 @@ const config = merge( base, {
         return module.context && module.context.indexOf( 'node_modules' ) !== -1
       }
     }),
+    /**
+     * Extract webpack runtime & manifest to avoid vendor chunk hash changing on every build.
+     */
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest'
+    }),
+    new VueSSRClientPlugin()
+  ]
+})
+
+// Only during production.
+if ( isProduction ) {
+
+  config.plugins.push(
     // auto generate service worker
     new SWPrecachePlugin({
       cacheId: 'project-acorn-ssr',
@@ -45,22 +60,9 @@ const config = merge( base, {
       }],
 
       dontCacheBustUrlsMatching: /./,
-      navigateFallback: '/',
-      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-      dynamicUrlToDependencies: {
-        '/': [
-          ...glob.sync( path.resolve( __dirname, '../dist/*.js' ) )
-        ]
-      }
-    }),
-    /**
-     * Extract webpack runtime & manifest to avoid vendor chunk hash changing on every build.
-     */
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest'
-    }),
-    new VueSSRClientPlugin()
-  ]
-})
+      navigateFallback: '/'
+    })
+  )
+}
 
 module.exports = config
