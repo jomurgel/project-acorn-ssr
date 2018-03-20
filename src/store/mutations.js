1feddc5f1f8dacf3a7abb6710e04903e46d0046a
jomurgel <dev@jomurgel.com>
2018-03-19 21:04:01 -0600

    rewrites post- and archive/blog-related mutations and adds category mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,41 +1,53 @@
 import Vue from 'vue'
 
 export default {
-  setActivePosts: ( state, { posts, count }) => {
-    Vue.set( state.posts, count, posts )
+  SET_PAGE: ( state, { slug, page }) => {
+    Vue.set( state.pages, slug, page )
   },
-  setPosts: ( state, [ posts, count ] ) => {
+  SET_MENUS: ( state, menus ) => {
+    state.navigation.menus = menus
+  },
+  SET_MENU_PULL_DATE: ( state, pullTime ) => {
+    state.navigation.pullDate = pullTime
+  },
+  SET_CATEGORIES: ( state, categories ) => {
+    state.categories = categories
+  },
+  SET_ARCHIVE: ( state, { type, ids, count }) => {
+
+    const pageCount = count - 1
+
+    Vue.set( state.archives[type].posts, pageCount, ids )
+
+  },
+  SET_POSTS: ( state, { posts }) => {
 
     posts.forEach( post => {
       if ( post ) {
-
-        // Set page number value.
-        post['pageNumber'] = count
-
-        // Set object data into array.
-        state.posts.push( post )
+        Vue.set( state.posts, post.id, post )
       }
     })
   },
-  setPost: ( state, { slug, post }) => {
-    state.posts.push( post )
+  SET_ACTIVE_TYPE: ( state, type ) => {
+    state.active.archive = type
   },
-  setPostCount: ( state, count ) => {
-    state.postCount = count
+  SET_ACTIVE_CAT: ( state, catId ) => {
+    state.active.category = catId
   },
-  setBlogPullStatus: ( state, pull ) => {
-    state.blogPull = pull
+  SET_ACTIVE_POST: ( state, id ) => {
+    state.active.post = id
   },
-  setBlogPullTimeStamp: ( state, pullTime ) => {
-    state.blogPullTime = pullTime
+  SET_POST: ( state, { post }) => {
+    if ( post ) {
+      Vue.set( state.posts, post.id, post )
+    }
   },
-  setPage: ( state, { slug, page }) => {
-    Vue.set( state.pages, slug, page )
+  SET_POST_COUNT: ( state, { type, postCount }) => {
+    state.archives[type].postCount = postCount
   },
-  setMenus: ( state, menus ) => {
-    state.menus = menus
-  },
-  setMenuPullTimeStamp: ( state, pullTime ) => {
-    state.menuPullTime = pullTime
+  SET_ARCHIVE_PULL_DATE: ( state, { type, count, date }) => {
+    const pageCount = count - 1
+
+    Vue.set( state.archives[type].pullDate, pageCount, date )
   }
 }
