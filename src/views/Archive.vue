8dc8fc359f3db1e1319277fa362c419380f871c8
jomurgel <dev@jomurgel.com>
2018-03-19 21:04:53 -0600

    renames Blog.vue to Archive.vue and adds additional helpers to function as blog archive (index/posts index) as well as a category archive

--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="posts">
+    <pagination :current-page="page" :max-page="maxPage" :has-more="hasMore" :route-name="$route.params.slug || 'blog'" />
+    <hr/>
     <transition name="fade" mode="out-in">
       <div class="post-list" :key="page" v-if="page > 0">
-        <transition-group tag="ul" name="post">
+        <transition-group tag="ul" name="post" :key="page.id">
           <li v-for="post in posts" :key="post.id" class="post">
             <h2>
               <router-link :to="'/blog/' + post.slug"> {{ post.title }}</router-link>
@@ -12,14 +14,16 @@
         </transition-group>
       </div>
     </transition>
-    <hr/>
-    <pagination :current-page="page" :max-page="maxPage" :has-more="hasMore" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import pagination from './components/Pagination'
+// import createStore from '../store'
+
+// Create router.
+// const store = createStore()
 
 export default {
   components: {
@@ -34,7 +38,9 @@ export default {
     return meta
   },
   asyncData({ store, route }) {
-    return store.dispatch( 'getPosts', parseInt( route.params.page ) || 1 )
+    const payload = { type: route.params.type || 'blog', count: parseInt( route.params.page ) || 1 }
+
+    return store.dispatch( 'getPosts', payload )
   },
   computed: {
     ...mapGetters({
@@ -44,11 +50,20 @@ export default {
       return parseInt( this.$route.params.page ) || 1
     },
     maxPage() {
-      return Math.ceil( this.$store.state.postCount / this.$store.state.postsPerPage )
+      return Math.ceil( this.$store.state.archives[this.$store.state.active.archive].postCount / this.$store.state.postsPerPage )
     },
     hasMore() {
       return this.page < this.maxPage
     }
+  },
+  beforeRouteEnter( to, from, next ) {
+    next( vm => {
+      const { asyncData } = vm.$options
+      asyncData({
+        store: vm.$store,
+        route: to
+      }).then( next ).catch( next )
+    })
   }
 }
 </script>
